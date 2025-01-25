0329e77e10f7c3c4df9885dc7e88206de2064f77
emmapalim <emma.palim@gmail.com>
2025-01-25 14:16:37 -0500

    added this is the best team

--- 2practiceapp/luckyapp/src/App.js
+++ 2practiceapp/luckyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello world
+          hello world! this is the best team
         </p>
         <a
           className="App-link"
